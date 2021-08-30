b60b816597af647e884f5c17b37c0b3607b9d45d
Iskandar Kurbanov <iskandarkurbanov@Iskandars-MBP.hitronhub.home>
2021-08-30 17:01:05 -0400

    added hero section to home page

---
+++ components/Hero.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+
+export default function Hero() {
+  return (
+    <div className="my-48 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-72 text-center">
+      <h1 className="font-extrabold text-gray-900">
+        <p className="text-xl sm:text-3xl md:text-4xl">Shopify + Next.js + Tailwind CSS:</p>
+        <p className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-4xl sm:text-6xl md:text-7xl">Modern eCommerce</p>
+      </h1>
+      <h2 className="mt-3 max-w-md mx-auto text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl">
+        Build the eCommerce Revolution.
+      </h2>
+      <div className="mt-5 max-w-md mx-auto flex justify-center items-center md:mt-8">
+        <Link href="#">
+          <a className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium py-3 border-transparent rounded-md text-white bg-gray-900 hover:bg-gray-800">
+            Enroll Now
+          </a>
+        </Link>
+        <Link href="#">
+          <a className="inline-flex items-center font-semibold text-gray-900 hover:text-gray-800">
+            Learn more
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { getProductsInCollection } from "../lib/shopify"
 import ProductList from "../components/ProductList"
+import Hero from "../components/Hero"
 
 
 export default function Home({ products }) {
 
   return (
-    <div className="text-3xl">
+    <div className="">
+      <Hero />
       <ProductList products={products} />
     </div>
   )
