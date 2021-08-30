4d611285557757c1b1dc2295320a47627a6f0274
Iskandar Kurbanov <iskandarkurbanov@Iskandars-MBP.hitronhub.home>
2021-08-30 18:04:14 -0400

    added basic seo

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import { getProductsInCollection } from "../lib/shopify"
 import ProductList from "../components/ProductList"
 import Hero from "../components/Hero"
+import Head from 'next/head'
 
 
 export default function Home({ products }) {
 
   return (
     <div className="">
+      <Head>
+        <title>Modern eCommerce Course</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <meta name="description" content="Modern eCommerce Development Course focusing on Shopify, Next.js, TailwindCSS, GraphQL. Additional topics include Storefront API, Static Site Generation, getStaticPaths, getStaticProps and more." />
+        <meta property="og:title" content="Modern eCommerce Course" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.buildnextshop.com" />
+        <meta property="og:image" content="https://www.buildnextshop.com/share.png" />
+        <meta property="og:description"
+          content="Modern eCommerce Development Course focusing on Shopify, Next.js, TailwindCSS, GraphQL. Additional topics include Storefront API, Static Site Generation, getStaticPaths, getStaticProps and more." />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Modern eCommerce Course" />
+      </Head>
       <Hero />
       <ProductList products={products} />
     </div>
