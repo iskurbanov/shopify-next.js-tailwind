a1eb07f1e56709f49cb981f5da7e2e3d59d17ebb
Iskandar Kurbanov <iskandarkurbanov@iskandars-mbp.home>
2021-10-06 14:43:51 -0400

    clean up

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="my-48 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-72 text-center">
       <h1 className="font-extrabold text-gray-900">
-        <p className="text-xl sm:text-3xl md:text-4xl">Shopify + Next.js + Tailwind CSS:</p>
+        <p className="text-xl sm:text-3xl md:text-4xl">Shopify + Next.js + Tailwind:</p>
         <p className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-4xl sm:text-6xl md:text-7xl">Modern eCommerce</p>
       </h1>
       <h2 className="mt-3 max-w-md mx-auto text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl">
